0ea815994d012dd55a2af6daa420313c13aa5ee8
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-18 09:52:00 -0500

    Created BaseSimpleList component

---
+++ src/components/ui/BaseSimpleList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="element in elements" :key="element">
+      {{ element }}
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    elements: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+}
+</style>
