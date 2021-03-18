694f636c276fce46463dc3792d5c7e64031fc20c
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-18 09:54:23 -0500

    Added combo module

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
 
+import comboModule from "./modules/combo/index";
 import navigationModule from "./modules/navigation/index";
 
 const store = createStore({
   modules: {
+    combo: comboModule,
     navigation: navigationModule,
   },
 });
