75bd431444e2d32bb2cfb57cea15d7f58ab672c4
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-24 10:09:07 -0500

    Created regexp.js util

---
+++ src/utils/regexp.js
@@ -0,0 +1,6 @@
+const RegExpValues = {
+  VALID_EMAIL: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+};
+
+export default RegExpValues;
+export const { VALID_EMAIL } = RegExpValues;
