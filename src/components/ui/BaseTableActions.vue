9d138a62b5e6d75ba5675fb3822663fa68b070b7
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-26 10:45:33 -0500

    Created BaseTableActions

---
+++ src/components/ui/BaseTableActions.vue
@@ -0,0 +1,57 @@
+<template>
+  <ul>
+    <li
+      v-for="action in actions"
+      :key="action.name"
+      @click="selectAction(action.name)"
+    >
+      <a href="#">
+        <span>{{ action.name }}</span>
+        <base-icon :icon="action.icon.name" :type="action.icon.type" />
+      </a>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  emits: ["selectAction"],
+  props: {
+    actions: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    selectAction(actionName) {
+      this.$emit("selectAction", actionName);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+ul {
+  list-style: none;
+  display: block;
+
+  a {
+    padding: 0.5rem 1rem;
+
+    color: $app-primary-font-color;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    text-decoration: none;
+
+    span {
+      white-space: initial;
+      text-align: start;
+      margin-left: 5px;
+    }
+
+    &:hover {
+      background-color: $app-primary-color;
+      color: $app-secondary-font-color;
+    }
+  }
+}
+</style>
