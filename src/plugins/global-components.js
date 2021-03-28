c53004018d0b060d6bd2a039864e3781916f58ab
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-27 19:28:20 -0500

    Added BaseBadge

--- src/plugins/global-components.js
+++ src/plugins/global-components.js
@@ -14,6 +14,7 @@ import BasePopover from "../components/ui/BasePopover";
 import BaseSimpleList from "../components/ui/BaseSimpleList";
 import BasePagination from "../components/ui/BasePagination";
 import BaseTableActions from "../components/ui/BaseTableActions";
+import BaseBadge from "../components/ui/BaseBadge";
 
 const addComponents = (app) => {
   app.component("font-awesome-icon", FontAwesomeIcon);
@@ -32,6 +33,7 @@ const addComponents = (app) => {
   app.component("base-simple-list", BaseSimpleList);
   app.component("base-pagination", BasePagination);
   app.component("base-table-actions", BaseTableActions);
+  app.component("base-badge", BaseBadge);
 };
 
 export { addComponents };
