919cc7959456b1a450751133cb7e21491f932bda
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-29 17:59:46 -0500

    Added logic

--- src/views/home/views/UsersView.vue
+++ src/views/home/views/UsersView.vue
@@ -1,16 +1,31 @@
 <template>
   <base-card>
     <template #header>
-      <h1>Lista de usuarios</h1>
-      <base-button type="button" color="success">Nuevo Usuario</base-button>
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-8 col-lg-10">
+          <h1>Lista de usuarios</h1>
+        </div>
+        <base-button
+          type="button"
+          color="success"
+          @click="toggleCreateUserModal"
+          class="col-12 col-sm-12 col-md-4 col-lg-2"
+        >
+          Nuevo Usuario
+        </base-button>
+      </div>
     </template>
-    <users-filter />
+    <users-filter @filter="searchUsers" />
   </base-card>
   <base-card>
     <users-table />
   </base-card>
-  <base-modal title="Nuevo Usuario" :open="true">
-    <create-user-form />
+  <base-modal
+    title="Nuevo Usuario"
+    :open="isCreateUserModalOpen"
+    @close="toggleCreateUserModal"
+  >
+    <create-user-form @submit-create="closeModalAndCreateUser" />
   </base-modal>
 </template>
 <script>
@@ -24,10 +39,34 @@ export default {
     UsersTable,
     CreateUserForm,
   },
+  data() {
+    return {
+      isCreateUserModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateUserModal() {
+      this.isCreateUserModalOpen = !this.isCreateUserModalOpen;
+    },
+    searchUsers(filters) {
+      // TODO: Search Users
+      console.log(filters);
+    },
+    closeModalAndCreateUser(userData) {
+      this.toggleCreateUserModal();
+      // TODO: Create User
+      console.log(userData);
+    },
+  },
 };
 </script>
 <style lang="scss" scoped>
 @include form-group;
 @include responsive-grid;
 @include layout;
+
+h1 {
+  font-size: 1.5rem;
+  font-weight: 300;
+}
 </style>
