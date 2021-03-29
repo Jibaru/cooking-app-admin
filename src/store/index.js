b1c9668c7cc2f0f5f1c5d6ff9504c87512d520da
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-29 17:08:34 -0500

    Added finder module

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { createStore } from "vuex";
 
 import comboModule from "./modules/combo/index";
+import finderModule from "./modules/finder/index";
 import navigationModule from "./modules/navigation/index";
 
 const store = createStore({
   modules: {
     combo: comboModule,
+    finder: finderModule,
     navigation: navigationModule,
   },
 });
