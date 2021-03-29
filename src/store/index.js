978168ab5e7536c3ead307dd14ec8c60446affcf
Ignacio <ignacioruedaboada@hotmail.com>
2021-03-29 17:18:20 -0500

    Added user module

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import { createStore } from "vuex";
 import comboModule from "./modules/combo/index";
 import finderModule from "./modules/finder/index";
 import navigationModule from "./modules/navigation/index";
+import userModule from "./modules/user/index";
 
 const store = createStore({
   modules: {
     combo: comboModule,
     finder: finderModule,
     navigation: navigationModule,
+    user: userModule,
   },
 });
 
