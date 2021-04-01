3c0bc9cc2010b03070d57d08c2d9143253671adb
Ignacio <ignacioruedaboada@hotmail.com>
2021-04-01 14:57:28 -0500

    Added findIngredients action

--- src/store/modules/finder/actions.js
+++ src/store/modules/finder/actions.js
@@ -1,1 +1,11 @@
-export default {};
+export default {
+  async findIngredients(context, { value }) {
+    // TODO: find ingredients
+
+    const findedIngredients = context.getters.ingredients.filter((option) =>
+      option.name.includes(value)
+    );
+
+    context.commit("setFilteredIngredients", { findedIngredients });
+  },
+};
