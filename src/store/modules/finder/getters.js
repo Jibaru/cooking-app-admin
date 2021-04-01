a5976248b7e6d7b6ede29857eebe2b525d72cc97
Ignacio <ignacioruedaboada@hotmail.com>
2021-04-01 14:56:18 -0500

    Created ingredient, ingredientValues getters

--- src/store/modules/finder/getters.js
+++ src/store/modules/finder/getters.js
@@ -1,3 +1,13 @@
 export default {
   users: (state) => state.users,
+  ingredients: (state) => state.ingredients,
+  ingredientValues(state) {
+    // FIXME: Change filteredIngredients to ingredients when create services
+    return state.filteredIngredients.map((ingredient) => {
+      return {
+        key: ingredient.id,
+        value: ingredient.name,
+      };
+    });
+  },
 };
