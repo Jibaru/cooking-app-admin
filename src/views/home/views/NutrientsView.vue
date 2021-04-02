db257e23644ed23e9df255f15b3482107c45f11a
Ignacio <ignacioruedaboada@hotmail.com>
2021-04-02 11:23:03 -0500

    Created NutrientsView

--- src/views/home/views/NutrientsView.vue
+++ src/views/home/views/NutrientsView.vue
@@ -1,3 +1,72 @@
 <template>
-  <h1>NutrientsView</h1>
+  <base-card>
+    <template #header>
+      <div class="row">
+        <div class="col-12 col-sm-12 col-md-8 col-lg-10">
+          <h1>Lista de nutrientes</h1>
+        </div>
+        <base-button
+          type="button"
+          color="success"
+          @click="toggleCreateNutrientModal"
+          class="col-12 col-sm-12 col-md-4 col-lg-2"
+        >
+          Nuevo Nutriente
+        </base-button>
+      </div>
+    </template>
+    <nutrients-filter @filter="searchNutrients" />
+  </base-card>
+  <base-card>
+    <nutrients-table />
+  </base-card>
+  <base-modal
+    title="Nuevo Nutriente"
+    :open="isCreateNutrientModalOpen"
+    @close="toggleCreateNutrientModal"
+  >
+    <create-nutrient-form @submit-create="closeModalAndCreateNutrient" />
+  </base-modal>
 </template>
+<script>
+import NutrientsFilter from "../../../components/nutrient/list/filter/NutrientsFilter";
+import NutrientsTable from "../../../components/nutrient/list/table/NutrientsTable";
+import CreateNutrientForm from "../../../components/nutrient/forms/CreateNutrientForm";
+
+export default {
+  components: {
+    NutrientsFilter,
+    NutrientsTable,
+    CreateNutrientForm,
+  },
+  data() {
+    return {
+      isCreateNutrientModalOpen: false,
+    };
+  },
+  methods: {
+    toggleCreateNutrientModal() {
+      this.isCreateNutrientModalOpen = !this.isCreateNutrientModalOpen;
+    },
+    searchNutrients(filters) {
+      // TODO: Search Nutrients
+      console.log(filters);
+    },
+    closeModalAndCreateNutrient(userData) {
+      this.toggleCreateNutrientModal();
+      // TODO: Create Nutrient
+      console.log(userData);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@include form-group;
+@include responsive-grid;
+@include layout;
+
+h1 {
+  font-size: 1.5rem;
+  font-weight: 300;
+}
+</style>
