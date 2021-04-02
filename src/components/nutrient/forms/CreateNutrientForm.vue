944cd6580d957ed0294807d83593392433844407
Ignacio <ignacioruedaboada@hotmail.com>
2021-04-02 11:21:46 -0500

    Created CreateNutrientForm

---
+++ src/components/nutrient/forms/CreateNutrientForm.vue
@@ -0,0 +1,95 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="row">
+      <div class="form-group col-12 col-sm-12 col-md-12 col-lg-12">
+        <label>Nombre</label>
+        <base-input
+          v-model="name.val"
+          :is-valid="name.isValid"
+          @blur="validateName"
+        />
+      </div>
+    </div>
+    <div class="flex-end">
+      <base-button type="submit" :color="submitButtonColor">
+        {{ submitButtonText }}
+      </base-button>
+    </div>
+  </form>
+</template>
+<script>
+import Validators from "@/utils/validators";
+
+export default {
+  emits: ["submit-create", "submit-edit"],
+  props: {
+    currentId: {
+      type: Number,
+    },
+    currentName: {
+      type: String,
+    },
+  },
+  data() {
+    return {
+      name: {
+        val: this.currentName,
+        isValid: true,
+      },
+    };
+  },
+  computed: {
+    isValidForm() {
+      return this.name.isValid;
+    },
+    isEditingNutrient() {
+      return (
+        this.currentId !== null &&
+        this.currentId !== undefined &&
+        typeof this.currentId === "number"
+      );
+    },
+    submitButtonColor() {
+      return this.isEditingNutrient ? "success" : "primary";
+    },
+    submitButtonText() {
+      return this.isEditingNutrient ? "Modificar Nutriente" : "Crear Nutriente";
+    },
+  },
+  methods: {
+    ...{
+      isEmpty: Validators.isEmpty,
+      isEmail: Validators.isEmail,
+    },
+    validateName() {
+      this.name.isValid = !this.isEmpty(this.name.val);
+    },
+    validateFields() {
+      this.validateName();
+    },
+    submitForm() {
+      this.validateFields();
+
+      if (!this.isValidForm) {
+        return;
+      }
+
+      const eventName = this.isEditingNutrient
+        ? "submit-edit"
+        : "submit-create";
+
+      this.$emit(eventName, {
+        ...(this.isEditingNutrient && {
+          nutrientId: this.currentId,
+        }),
+        name: this.name.val,
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@include form-group;
+@include responsive-grid;
+@include layout;
+</style>
