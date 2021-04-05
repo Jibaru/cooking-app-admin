4b84cb5c0dc8da78654744b631f30afdbccf078f
Ignacio <ignacioruedaboada@hotmail.com>
2021-04-05 16:08:40 -0500

    Added notification module

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import { createStore } from "vuex";
 import comboModule from "./modules/combo/index";
 import finderModule from "./modules/finder/index";
 import navigationModule from "./modules/navigation/index";
+import notificationModule from "./modules/notification/index";
 import nutrientModule from "./modules/nutrient/index";
 import userModule from "./modules/user/index";
 
@@ -11,6 +12,7 @@ const store = createStore({
     combo: comboModule,
     finder: finderModule,
     navigation: navigationModule,
+    notification: notificationModule,
     nutrient: nutrientModule,
     user: userModule,
   },
