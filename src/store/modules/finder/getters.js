35bb1c5504d7d3e6af063ab134d87b5a8cec0abf
Ignacio <ignacioruedaboada@hotmail.com>
2021-04-05 16:10:00 -0500

    Added userValues

--- src/store/modules/finder/getters.js
+++ src/store/modules/finder/getters.js
@@ -1,5 +1,14 @@
 export default {
   users: (state) => state.users,
+  userValues(state) {
+    // FIXME: Change filteredUsers to users when create services
+    return state.filteredUsers.map((user) => {
+      return {
+        key: user.id,
+        value: user.firstName + " " + user.lastName,
+      };
+    });
+  },
   ingredients: (state) => state.ingredients,
   ingredientValues(state) {
     // FIXME: Change filteredIngredients to ingredients when create services
