8be13b31ee24fcd518824d2aae0760ce93764de6
jibaru <xxnacho1997xx@gmail.com>
2021-04-06 18:17:55 -0500

    Added recipeCuisine module

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,7 @@ import finderModule from "./modules/finder/index";
 import navigationModule from "./modules/navigation/index";
 import notificationModule from "./modules/notification/index";
 import nutrientModule from "./modules/nutrient/index";
+import recipeCuisineModule from "./modules/recipe-cuisine/index";
 import recipeTagModule from "./modules/recipe-tag/index";
 import userModule from "./modules/user/index";
 
@@ -15,6 +16,7 @@ const store = createStore({
     navigation: navigationModule,
     notification: notificationModule,
     nutrient: nutrientModule,
+    recipeCuisine: recipeCuisineModule,
     recipeTag: recipeTagModule,
     user: userModule,
   },
