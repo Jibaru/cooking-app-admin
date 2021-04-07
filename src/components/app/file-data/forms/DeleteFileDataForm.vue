f035dad740a012e809c5676e12e5d3d3dd35d617
jibaru <xxnacho1997xx@gmail.com>
2021-04-07 13:50:00 -0500

    Created DeleteFilerDataForm

---
+++ src/components/app/file-data/forms/DeleteFileDataForm.vue
@@ -0,0 +1,61 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="info">
+      <strong> ¿Desea eliminar el archivo id: {{ fileDataId }}? </strong>
+      <p><b>Nombre:</b> {{ fileDataName }}</p>
+    </div>
+    <div class="flex-end">
+      <base-button type="submit">
+        Aceptar
+      </base-button>
+    </div>
+  </form>
+</template>
+<script>
+export default {
+  emits: ["submit"],
+  props: {
+    fileDataId: {
+      type: Number,
+      required: true,
+    },
+    fileDataName: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    submitForm() {
+      this.$emit("submit", {
+        id: this.fileDataId,
+      });
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+@include form-group;
+@include responsive-grid;
+@include layout;
+
+.info {
+  padding: 0.5rem;
+
+  strong {
+    font-weight: 500;
+    text-align: center;
+    margin-bottom: 1rem;
+    display: block;
+  }
+
+  b {
+    font-weight: 700;
+  }
+
+  p {
+    display: flex;
+    justify-content: space-between;
+    text-align: right;
+  }
+}
+</style>
