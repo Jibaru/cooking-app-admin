738f5664ca40f2600521d0ce0107aa8778ed4430
jibaru <xxnacho1997xx@gmail.com>
2021-04-10 19:17:23 -0500

    Created BaseEmptyMessage

---
+++ src/components/ui/BaseEmptyMessage.vue
@@ -0,0 +1,23 @@
+<template>
+  <p>
+    <base-icon type="fas" icon="sad-tear" class="secondary" /> <br />
+    {{ message }}
+  </p>
+</template>
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      default: "Sin elementos",
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+p {
+  text-align: center;
+  color: $app-secondary-color;
+  font-size: 0.7rem;
+}
+</style>
