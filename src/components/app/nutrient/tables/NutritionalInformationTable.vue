8afc899831365cd54b00a869c71f4763d8670ae1
jibaru <xxnacho1997xx@gmail.com>
2021-04-10 20:06:07 -0500

    Created NutritionalInformationTable

---
+++ src/components/app/nutrient/tables/NutritionalInformationTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <base-table>
+    <template #head>
+      <base-cell tag="th">ID</base-cell>
+      <base-cell tag="th">Nutriente</base-cell>
+      <base-cell tag="th">Cantidad</base-cell>
+      <base-cell tag="th">Unidad</base-cell>
+      <base-cell tag="th">Acciones</base-cell>
+    </template>
+    <template #body>
+      <tr
+        v-for="(nutritionalInformation, index) in modelValue"
+        :key="nutritionalInformation.id"
+        :class="{ 'striped-row': (index + 1) % 2 == 0 }"
+      >
+        <base-cell tag="td">
+          {{ nutritionalInformation.nutrientId }}
+        </base-cell>
+        <base-cell tag="td">
+          {{ nutritionalInformation.nutrientName }}
+        </base-cell>
+        <base-cell tag="td">
+          {{ nutritionalInformation.amount }}
+        </base-cell>
+        <base-cell tag="td">
+          <base-badge>
+            {{ nutritionalInformation.unit }}
+          </base-badge>
+        </base-cell>
+        <base-cell tag="td">
+          <base-button
+            type="button"
+            color="danger"
+            @click="removeNutritionalInformation(index)"
+          >
+            <base-icon icon="trash-alt" type="fas" />
+          </base-button>
+        </base-cell>
+      </tr>
+    </template>
+  </base-table>
+</template>
+<script>
+export default {
+  emits: ["update:modelValue"],
+  props: {
+    modelValue: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    removeNutritionalInformation(index) {
+      const nutritionalInformations = [...this.modelValue];
+      nutritionalInformations.splice(index, 1);
+      this.$emit("update:modelValue", nutritionalInformations);
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.striped-row {
+  background-color: $app-striped-row-color;
+  color: $app-striped-row-font-color;
+}
+</style>
